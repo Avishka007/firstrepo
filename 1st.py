e88b3e955154ccd93ed92398c84c4905b52ce049
Avishka007 <telanb@aol.co.uk>
2018-02-25 02:24:08 +0530

    added text

--- 1st.py
+++
@@ -1,9 +0,0 @@
-x = input ("enter your first name: ")
-y = input ("enter your secoend name: ")
-z = input ("enter your family name:  ")
-
-a = (x+ " " + y + " " + z + " " )
-print (a)
-print ("welcome" +" "+ x + "!")
-
-input ("press enter to exit")
